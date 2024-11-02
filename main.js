22a5bfd15cdc2a04388831dea3bdca841c975fbb
unknown <nkhub2018@agruni.edu.ge>
2024-11-03 02:35:24 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+////////////////////////////
+// task 1
+//////////////////////////////////////////////
+
+
+// function same(a,b){
+//     if(a==b){
+//         return 'ტოლია'
+//     } else {
+//         return 'არ არის ტოლი'
+//     } 
+// }
+
+// console.log(same(5,5))
+
+
+
+////////////////////////////
+// task 2
+//////////////////////////////////////////////
+
+// function temp(x){
+ 
+//  version 1
+
+//    if(typeof x != 'number'){
+//         return false
+//    } else {
+//         return (x - 32) / 1.8
+//    }
+
+//  version 2 
+
+
+// const y = (x - 32) / 1.8
+// if(typeof x != 'number'){
+//     return false
+// }
+// return y
+
+// }
+
+// console.log(temp(10))
+
+
+
+////////////////////////////
+// task 3
+//////////////////////////////////////////////
+
+// function task(a,b,x){
+//     if(typeof a != 'number' || typeof b != 'number' ) {
+//         return false
+//     } 
+//     switch (x) {
+//         case '+':
+//         return  a + b 
+//         case '-':
+//         return  a - b 
+//         case '*':
+//         return  a * b 
+//         case '/':
+//         return  a / b 
+//         default: return false
+//     }
+// }
+
+// console.log(task(5,2,'*'))
